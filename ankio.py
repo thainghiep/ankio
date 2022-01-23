72dc12d5007042039d81d1626711cdf09c0c2b3b
thai nghiep <75659318+tnghiep@users.noreply.github.com>
2022-01-23 20:58:46 +0700

    updata

--- ankio.py
+++ ankio.py
@@ -1,18 +1,22 @@
-def ClozeExample(Word,Example):
+def Cloze(Word,Example):
   Cloze = "{{c1::" + Word + "}}"
   return Example.replace(Word,Cloze)
 
-def Hint(Word):
-  First = Word[0:1]
-  Last = Word[len(Word)-1:len(Word)]
-  suggest = ""
-  for i in range(len(Word)-2):
-    suggest = suggest + "_ "
-  suggest = suggest.strip()
-  return First + suggest + Last
+''' Hint(Word)
+moon            ==>        m_ _ _
+to the moon     ==>        t_ t_ _ m_ _ _
+'''
+def HintNoSpace(Word):
+    underscore = ""
+    for i in range(len(Word) - 1):
+        underscore = underscore + "_ "
+    underscore = underscore.strip()
+    return Word[0] + underscore
 
-def HintV2(Word):
-  suggest = ""
-  for i in range(len(Word)-2):
-    suggest = suggest + "_ "
-  suggest = suggest.strip()
+def Hint(Word):
+    SubWord = Word.split(" ")
+    hint = ""
+    for i in range(len(SubWord) - 1):
+        hint = hint + HintNoSpace(SubWord[i]) + " "
+    hint = hint + HintNoSpace(SubWord[len(SubWord) - 1])
+    return hint
--- test.py
+++ test.py
@@ -1,60 +1,8 @@
-'''
-example              
-e_ _ _ _ _ _
+import ankio 
 
-to the moon          
-t_ t_ _ m_ _ _
-'''
-word = "to the moon"
-word2 = "moon"
+word = "moon"
+example = "to the moon"
 
-#print(len(word))
-print(word)
+print(ankio.Hint(word))
+print(ankio.Cloze(word, example))
 
-def HintNoSpace(Word):
-    underscore = ""
-    for i in range(len(Word) - 1):
-        underscore = underscore + "_ "
-    underscore = underscore.strip()
-    return Word[0] + underscore
-
-def HintSpace(Word):
-    return HintNoSpace(Word[0:len(Word) - 1]) + " "
-'''
-print(HintNoSpace("moon"))
-print(HintSpace("moon "))
-'''    
-
-def CheckSpace(Word):
-    for i in range(len(Word)):
-        if (Word[i] == " "):
-            return i
-    return -1
-
-#print(CheckSpace(word))
-
-SubWord = word.split(" ")
-
-print(len(SubWord))
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-to the moon
-
-t_
-t_ _
-m_ _ _
-
-t_ t_ _ m_ _ _ 
-
-'''
