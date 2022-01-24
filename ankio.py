3e55defd215e4936211ef37df51f777f1f92506b
Thái Bá Nghiệp <75659318+tbnghiep@users.noreply.github.com>
2022-01-24 14:48:03 +0700

    updata module

--- ankio.py
+++ ankio.py
@@ -1,22 +1,25 @@
+'''     Cloze(Word,Example)
+    moon | to the moon
+=>  to the {{c1::moon}}
+'''
 def Cloze(Word,Example):
   Cloze = "{{c1::" + Word + "}}"
   return Example.replace(Word,Cloze)
 
-''' Hint(Word)
+
+'''  Hint(Word):
 moon            ==>        m_ _ _
 to the moon     ==>        t_ t_ _ m_ _ _
 '''
 def HintNoSpace(Word):
-    underscore = ""
-    for i in range(len(Word) - 1):
-        underscore = underscore + "_ "
-    underscore = underscore.strip()
-    return Word[0] + underscore
+    underscore = ["_"] * (len(Word) - 1)
+    space = " "
+    return Word[0] + space.join(underscore)
 
 def Hint(Word):
+    Word = Word.strip()
     SubWord = Word.split(" ")
-    hint = ""
-    for i in range(len(SubWord) - 1):
-        hint = hint + HintNoSpace(SubWord[i]) + " "
-    hint = hint + HintNoSpace(SubWord[len(SubWord) - 1])
-    return hint
+    for i in range(len(SubWord)):
+        SubWord[i] = HintNoSpace(SubWord[i]) 
+    delimiter = " "
+    return  delimiter.join(SubWord)
--- test3.py
+++
@@ -1,7 +0,0 @@
-word = "moon"
-
-underscore = ["_"] * (len(word) - 1)
-space = " "
-hidden
-
-print(underscore)
